8297ae1177d39c160ae44f57339d438af6609486
SHAREA SOBUJ <shareasobuj9829@gmail.com>
2026-06-22 13:01:58 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 // ১. Firebase কনফিগারেশন (আপনার Firebase কনসোল থেকে এগুলো পরিবর্তন করুন)
 const firebaseConfig = {
-    apiKey: "YOUR_FIREBASE_API_KEY",
-    authDomain: "YOUR_FIREBASE_AUTH_DOMAIN",
-    projectId: "YOUR_FIREBASE_PROJECT_ID",
-    storageBucket: "YOUR_FIREBASE_STORAGE_BUCKET",
-    messagingSenderId: "YOUR_FIREBASE_MESSAGING_SENDER_ID",
-    appId: "YOUR_FIREBASE_APP_ID"
+      apiKey: "AIzaSyCEdRuYGcek5H0i9esjRF6SUzfbRAF_6IY",
+  authDomain: "privateapp-84e1f.firebaseapp.com",
+  projectId: "privateapp-84e1f",
+  storageBucket: "privateapp-84e1f.firebasestorage.app",
+  messagingSenderId: "328201856784",
+  appId: "1:328201856784:web:bdfeb67631acb323b52795",
+  measurementId: "G-B0MYHMC944"
 };
 // --- CONFIGURATION ---
 const IMGBB_API_KEY = "6872e0a43175b6750389b34f632f4d99"; // আপনার আসল ImgBB API Key বসানো হলো
